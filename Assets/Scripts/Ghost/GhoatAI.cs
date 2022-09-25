485b733c81cdf1137b8577062cf09bf46e23e762
Heymin <wjsgpals0705@gmail.com>
2022-09-26 07:09:24 +0900

    [Fix] Ghost AI scripts 추기(미구현)

---
+++ Assets/Scripts/Ghost/GhoatAI.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Map
+{
+    public Map(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
+
+    public bool isWall;
+    public Map ParentNode;
+
+    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
+    public int x, y, G, H;
+    public int F { get { return G + H; } }
+}
+
+
+public class GhoatAI: MonoBehaviour
+{
+    private bool _isWall;
+
+    public GameObject target;
+    public Vector2Int bottomLeft, topRight;
+    private Vector2Int startPos, targetPos;
+    public List<Map> FinalNodeList;
+
+    int sizeX, sizeY;
+    Map[,] NodeArray;
+    Map StartNode, TargetNode, CurNode;
+    List<Map> OpenList, ClosedList;
+
+    MapManager _mapManager;
+
+    private void Start()
+    {
+        startPos = new Vector2Int((int)gameObject.transform.position.x, (int)gameObject.transform.position.y);
+        targetPos = new Vector2Int((int)target.transform.position.x, (int)target.transform.position.y);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void PathFinding()
+    {
+        // NodeArray의 크기 정해주고, isWall, x, y 대입
+        sizeX = 30;
+        sizeY = 30;
+        NodeArray = new Map[sizeX, sizeY];
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                _isWall = false;
+                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f))
+                {
+                    if (_mapManager.CheckDirectionToGo(new Vector2(i + bottomLeft.x, j + bottomLeft.y), MapManager.Direction.None))
+                    {
+                        _isWall = true;
+                    }
+                }
+
+                NodeArray[i, j] = new Map(_isWall, i + bottomLeft.x, j + bottomLeft.y);
+            }
+        }
+
+
+        // 시작과 끝 노드, 열린리스트와 닫힌리스트, 마지막리스트 초기화
+        StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.y - bottomLeft.y];
+        TargetNode = NodeArray[targetPos.x - bottomLeft.x, targetPos.y - bottomLeft.y];
+
+        OpenList = new List<Map>() { StartNode };
+        ClosedList = new List<Map>();
+        FinalNodeList = new List<Map>();
+
+        while (OpenList.Count > 0)
+        {
+            // 열린리스트 중 가장 F가 작고 F가 같다면 H가 작은 걸 현재노드로 하고 열린리스트에서 닫힌리스트로 옮기기
+            CurNode = OpenList[0];
+            for (int i = 1; i < OpenList.Count; i++)
+                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H) CurNode = OpenList[i];
+
+            OpenList.Remove(CurNode);
+            ClosedList.Add(CurNode);
+
+
+            // 마지막
+            if (CurNode == TargetNode)
+            {
+                Map TargetCurNode = TargetNode;
+                while (TargetCurNode != StartNode)
+                {
+                    FinalNodeList.Add(TargetCurNode);
+                    TargetCurNode = TargetCurNode.ParentNode;
+                }
+                FinalNodeList.Add(StartNode);
+                FinalNodeList.Reverse();
+
+                for (int i = 0; i < FinalNodeList.Count; i++) print(i + "번째는 " + FinalNodeList[i].x + ", " + FinalNodeList[i].y);
+                return;
+            }
+
+            // ↑ → ↓ ←
+            OpenListAdd(CurNode.x, CurNode.y + 1);
+            OpenListAdd(CurNode.x + 1, CurNode.y);
+            OpenListAdd(CurNode.x, CurNode.y - 1);
+            OpenListAdd(CurNode.x - 1, CurNode.y);
+        }
+    }
+
+    void OpenListAdd(int checkX, int checkY)
+    {
+        // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
+        {
+
+            // 이웃노드에 넣고, 직선은 10, 대각선은 14비용
+            Map NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
+            int MoveCost = CurNode.G + (CurNode.x - checkX == 0 || CurNode.y - checkY == 0 ? 10 : 14);
+
+
+            // 이동비용이 이웃노드G보다 작거나 또는 열린리스트에 이웃노드가 없다면 G, H, ParentNode를 설정 후 열린리스트에 추가
+            if (MoveCost < NeighborNode.G || !OpenList.Contains(NeighborNode))
+            {
+                NeighborNode.G = MoveCost;
+                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(NeighborNode.y - TargetNode.y)) * 10;
+                NeighborNode.ParentNode = CurNode;
+
+                OpenList.Add(NeighborNode);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (FinalNodeList.Count != 0) for (int i = 0; i < FinalNodeList.Count - 1; i++)
+        {
+            Gizmos.DrawLine(new Vector2(FinalNodeList[i].x, FinalNodeList[i].y), new Vector2(FinalNodeList[i + 1].x, FinalNodeList[i + 1].y));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("RandomTarget"))
+        {
+            _mapManager.SpawnRandomTarget();
+        }
+    }
+
+}
+
+
