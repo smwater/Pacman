08944ffe2c63889ee57975f74030b59bb05807f3
Heymin <wjsgpals0705@gmail.com>
2022-09-25 19:00:15 +0900

    [Peat] Ranking UI 추가

---
+++ Assets/Scripts/Player/Ranking.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranking : MonoBehaviour
+{
+    void ScoreSet(float currentScore)
+    {
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
